dbfd098da033d292d4e0e0615496ff6bf50dc76b
hojin <sungsill150@gmail.com>
2022-06-03 23:47:14 +0900

    Implement Main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import com.fastcampus.javaoop.logic.BubbleSort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("[result]" + sort.sort(Arrays.asList(args)));
+
+
+    }
+}
